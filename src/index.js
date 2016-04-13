4e5b75d3dfcf232b616c735179ecf8ce49f2ebe3
Graham Mendick <graham.mendick@gmail.com>
2016-04-13 11:13:45 +0100

    Replaced director with navigation

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import 'todomvc-common';
 import {TodoModel} from './stores/todoModel';
-import ViewModel from './stores/viewModel';
+import ViewModel, {ALL_TODOS} from './stores/viewModel';
 import TodoApp from './components/todoApp.js';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {StateNavigator} from 'navigation';
 
 var todoModel = new TodoModel('mobx-react-todomvc');
 var viewModel = new ViewModel();
+var stateNavigator = new StateNavigator([
+	{key: 'todo', route: '{filter?}', defaults: {filter: ALL_TODOS}}
+]);
 
 ReactDOM.render(
-	<TodoApp todoModel={todoModel} viewModel={viewModel}/>,
+	<TodoApp
+		todoModel={todoModel}
+		viewModel={viewModel}
+		stateNavigator={stateNavigator}
+	/>,
 	document.getElementById('todoapp')
 );
