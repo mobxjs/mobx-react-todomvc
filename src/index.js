e5a9508d550d2a4a30c4f7cdbe99427b7d66b057
Graham Mendick <graham.mendick@gmail.com>
2016-04-13 11:15:40 +0100

    Changed from hash history to HTML5 history

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import ViewModel, {ALL_TODOS} from './stores/viewModel';
 import TodoApp from './components/todoApp.js';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {StateNavigator} from 'navigation';
+import {StateNavigator, HTML5HistoryManager} from 'navigation';
 
 var todoModel = new TodoModel('mobx-react-todomvc');
 var viewModel = new ViewModel();
 var stateNavigator = new StateNavigator([
 	{key: 'todo', route: '{filter?}', defaults: {filter: ALL_TODOS}}
-]);
+], new HTML5HistoryManager());
 
 ReactDOM.render(
 	<TodoApp
