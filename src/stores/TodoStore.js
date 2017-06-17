783742edb2d82470462f9afee9eb061aea1d4210
Andrey Kogut <andrey.kogut@uvoteam.com>
2017-06-17 18:01:19 +0300

    Check window.fetch availability

--- src/stores/TodoStore.js
+++ src/stores/TodoStore.js
@@ -20,7 +20,7 @@ export default class TodoStore {
 	subscribeServerToStore() {
 		reaction(
 			() => this.toJS(),
-			todos => fetch('/api/todos', {
+			todos => window.fetch && fetch('/api/todos', {
 				method: 'post',
 				body: JSON.stringify({ todos }),
 				headers: new Headers({ 'Content-Type': 'application/json' })
