4f7f931740b72630751fbb5d65455043de2e10c8
Filip Stepien <contact@filipstepien.com>
2018-06-25 16:07:37 -0400

    add tags into todoItem.js

--- src/components/todoOverview.js
+++ src/components/todoOverview.js
@@ -1,56 +1,52 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import {observer} from 'mobx-react';
-import { ACTIVE_TODOS, COMPLETED_TODOS } from '../constants';
+import React from "react";
+import PropTypes from "prop-types";
+import { observer } from "mobx-react";
+import { ACTIVE_TODOS, COMPLETED_TODOS } from "../constants";
 
-import TodoItem from './todoItem';
+import TodoItem from "./todoItem";
 
 @observer
 export default class TodoOverview extends React.Component {
-	render() {
-		const {todoStore, viewStore} = this.props;
-		if (todoStore.todos.length === 0)
-			return null;
-		return <section className="main">
-			<input
-				className="toggle-all"
-				type="checkbox"
-				onChange={this.toggleAll}
-				checked={todoStore.activeTodoCount === 0}
-			/>
-			<ul className="todo-list">
-				{this.getVisibleTodos().map(todo =>
-					(<TodoItem
-						key={todo.id}
-						todo={todo}
-						viewStore={viewStore}
-					/>)
-				)}
-			</ul>
-		</section>
-	}
+  render() {
+    const { todoStore, viewStore } = this.props;
+    if (todoStore.todos.length === 0) return null;
+    return (
+      <section className="main">
+        <input
+          className="toggle-all"
+          type="checkbox"
+          onChange={this.toggleAll}
+          checked={todoStore.activeTodoCount === 0}
+        />
+        <ul className="todo-list">
+          {this.getVisibleTodos().map(todo => (
+            <TodoItem key={todo.id} todo={todo} viewStore={viewStore} />
+          ))}
+        </ul>
+      </section>
+    );
+  }
 
-	getVisibleTodos() {
-		return this.props.todoStore.todos.filter(todo => {
-			switch (this.props.viewStore.todoFilter) {
-				case ACTIVE_TODOS:
-					return !todo.completed;
-				case COMPLETED_TODOS:
-					return todo.completed;
-				default:
-					return true;
-			}
-		});
-	}
+  getVisibleTodos() {
+    return this.props.todoStore.todos.filter(todo => {
+      switch (this.props.viewStore.todoFilter) {
+        case ACTIVE_TODOS:
+          return !todo.completed;
+        case COMPLETED_TODOS:
+          return todo.completed;
+        default:
+          return true;
+      }
+    });
+  }
 
-	toggleAll = (event) => {
-		var checked = event.target.checked;
-		this.props.todoStore.toggleAll(checked);
-	};
+  toggleAll = event => {
+    var checked = event.target.checked;
+    this.props.todoStore.toggleAll(checked);
+  };
 }
-
 
 TodoOverview.propTypes = {
-	viewStore: PropTypes.object.isRequired,
-	todoStore: PropTypes.object.isRequired
-}
+  viewStore: PropTypes.object.isRequired,
+  todoStore: PropTypes.object.isRequired
+};
--- src/stores/TodoStore.js
+++ src/stores/TodoStore.js
@@ -1,63 +1,65 @@
-import {observable, computed, reaction} from 'mobx';
-import TodoModel from '../models/TodoModel'
-import * as Utils from '../utils';
-
+import { observable, computed, reaction } from "mobx";
+import TodoModel from "../models/TodoModel";
+import * as Utils from "../utils";
 
 export default class TodoStore {
-	@observable todos = [];
+  @observable todos = [];
 
-	@computed get activeTodoCount() {
-		return this.todos.reduce(
-			(sum, todo) => sum + (todo.completed ? 0 : 1),
-			0
-		)
-	}
+  @computed
+  get activeTodoCount() {
+    return this.todos.reduce((sum, todo) => sum + (todo.completed ? 0 : 1), 0);
+  }
 
-	@computed get completedCount() {
-		return this.todos.length - this.activeTodoCount;
-	}
+  @computed
+  get completedCount() {
+    return this.todos.length - this.activeTodoCount;
+  }
 
-	subscribeServerToStore() {
-		reaction(
-			() => this.toJS(),
-			todos => window.fetch && fetch('/api/todos', {
-				method: 'post',
-				body: JSON.stringify({ todos }),
-				headers: new Headers({ 'Content-Type': 'application/json' })
-			})
-		);
-	}
+  subscribeServerToStore() {
+    reaction(
+      () => this.toJS(),
+      todos =>
+        window.fetch &&
+        fetch("/api/todos", {
+          method: "post",
+          body: JSON.stringify({ todos }),
+          headers: new Headers({ "Content-Type": "application/json" })
+        })
+    );
+  }
 
-	subscribeLocalstorageToStore() {
-		reaction(
-			() => this.toJS(),
-			todos => localStorage.setItem('mobx-react-todomvc-todos', JSON.stringify({ todos }))
-		);
-	}
+  subscribeLocalstorageToStore() {
+    reaction(
+      () => this.toJS(),
+      todos =>
+        localStorage.setItem(
+          "mobx-react-todomvc-todos",
+          JSON.stringify({ todos })
+        )
+    );
+  }
 
-	addTodo (title) {
-		this.todos.push(new TodoModel(this, Utils.uuid(), title, false));
-	}
+  //added tags to todo
+  addTodo(title) {
+    let tags = [];
+    this.todos.push(new TodoModel(this, Utils.uuid(), title, false, tags));
+  }
 
-	toggleAll (checked) {
-		this.todos.forEach(
-			todo => todo.completed = checked
-		);
-	}
+  toggleAll(checked) {
+    this.todos.forEach(todo => (todo.completed = checked));
+  }
 
-	clearCompleted () {
-		this.todos = this.todos.filter(
-			todo => !todo.completed
-		);
-	}
+  clearCompleted() {
+    this.todos = this.todos.filter(todo => !todo.completed);
+  }
 
-	toJS() {
-		return this.todos.map(todo => todo.toJS());
-	}
+  toJS() {
+    return this.todos.map(todo => todo.toJS());
+  }
 
-	static fromJS(array) {
-		const todoStore = new TodoStore();
-		todoStore.todos = array.map(item => TodoModel.fromJS(todoStore, item));
-		return todoStore;
-	}
+  static fromJS(array) {
+    const todoStore = new TodoStore();
+    todoStore.todos = array.map(item => TodoModel.fromJS(todoStore, item));
+    return todoStore;
+  }
 }
