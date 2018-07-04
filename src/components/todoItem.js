846fa0f4034d98645bd1b864e3e5db99dc7d9637
Filip Stepien <contact@filipstepien.com>
2018-07-04 07:10:43 -0400

    add todo tags and tag filtering

--- src/components/todoItem.js
+++ src/components/todoItem.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import ReactDOM from "react-dom";
 import PropTypes from 'prop-types';
-import {observer} from 'mobx-react';
-import {observable, expr} from 'mobx';
+import { observer } from 'mobx-react';
+import { observable, expr } from 'mobx';
 
 const ESCAPE_KEY = 27;
 const ENTER_KEY = 13;
@@ -11,10 +12,10 @@ export default class TodoItem extends React.Component {
 	@observable editText = "";
 
 	render() {
-		const {viewStore, todo} = this.props;
+		const { viewStore, todo } = this.props;
 		return (
 			<li className={[
-				todo.completed ? "completed": "",
+				todo.completed ? "completed" : "",
 				expr(() => todo === viewStore.todoBeingEdited ? "editing" : "")
 			].join(" ")}>
 				<div className="view">
@@ -27,7 +28,29 @@ export default class TodoItem extends React.Component {
 					<label onDoubleClick={this.handleEdit}>
 						{todo.title}
 					</label>
+					{todo.tags.map(tag => (
+						<span
+							key={todo.id + "-" + tag}
+							data-key={todo.id + "-" + tag}
+							style={todoTag}
+						>
+							{tag}
+						</span>
+					))}
 					<button className="destroy" onClick={this.handleDestroy} />
+					<button
+						name="tagSubmitBtn"
+						style={addBtnStyle}
+						onClick={this.handleTagSubmitByButton}
+					>+</button>
+					<input
+						ref="tagInput"
+						name="tagInput"
+						type="text"
+						placeholder="Enter Tag"
+						style={tagInput}
+						ref="tagField"
+						onKeyDown={this.handleTagInputKeyDown} />
 				</div>
 				<input
 					ref="editField"
@@ -40,6 +63,30 @@ export default class TodoItem extends React.Component {
 			</li>
 		);
 	}
+
+	handleTagInputKeyDown = event => {
+		if (event.keyCode !== ENTER_KEY) {
+			return;
+		}
+		event.preventDefault();
+		this.submitTag();
+	};
+
+	handleTagSubmitByButton = () => {
+		event.preventDefault();
+		this.submitTag();
+	};
+
+	submitTag = () => {
+		var val = ReactDOM.findDOMNode(this.refs.tagField)
+			.value.trim()
+			.replace(" ", "");
+		if (val) {
+			this.props.todo.addTag(val);
+			this.props.viewStore.addFilterTag(val);
+			ReactDOM.findDOMNode(this.refs.tagField).value = "";
+		}
+	};
 
 	handleSubmit = (event) => {
 		const val = this.editText.trim();
@@ -84,4 +131,42 @@ export default class TodoItem extends React.Component {
 TodoItem.propTypes = {
 	todo: PropTypes.object.isRequired,
 	viewStore: PropTypes.object.isRequired
+};
+
+const addBtnStyle = {
+	position: "absolute",
+	display: "inline",
+	background: "lightgreen",
+	right: "-210px",
+	top: "50%",
+	transform: "translateY(-50%)",
+	borderRadius: "50%",
+	width: "30px",
+	height: "30px",
+	boxShadow: "0 2px 4px 0 rgba(0, 0, 0, 0.2)",
+	cursor: "pointer"
+};
+
+const tagInput = {
+	position: "absolute",
+	display: "inline",
+	background: "white",
+	right: "-170px",
+	width: "150px",
+	top: "50%",
+	transform: "translateY(-50%)",
+	borderRadius: "4px",
+	border: "1px solid lightgrey",
+	height: "30px",
+	padding: "5px",
+	boxShadow: "0 2px 4px 0 rgba(0, 0, 0, 0.2)"
+};
+
+const todoTag = {
+	fontSize: 16,
+	margin: "5px",
+	border: "solid 1px grey",
+	padding: "6px 12px",
+	display: "inline-block",
+	borderRadius: 4
 };
